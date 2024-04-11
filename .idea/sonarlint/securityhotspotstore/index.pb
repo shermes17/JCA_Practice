234fc38805ef38767143ac61257524711f32a772
Sean Hermes <seanhermes617@gmail.com>
2024-04-11 18:58:19 -0400

    HW4 Complete

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,14 @@
+
+D
+Cryptography-HW4.iml,9/4/94d61f142340bb7b6337c4b29ba5a35f063e0823
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+;
+HW4/HW4.iml,1/1/11fe67edb81e0fb2db54134721c8808a819e1471
+>
+HW4/.gitignore,3/f/3f46faa32e1d59cf59de4c6b1df067d908d71ac0
+E
+HW4/src/P1_cipher.txt,e/1/e1690e1968206ea0f4cd04429957e6fd12d81acb
+@
+HW4/src/HW4.java,3/b/3b909f7638c65d9ebc1c32ac2e86ffbead8b7cd4
--- HW4/src/HW4.java
+++ HW4/src/HW4.java
@@ -9,6 +9,10 @@ import java.nio.file.Paths;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
+import java.nio.file.StandardOpenOption;
+
+
+
 public class HW4 {
 
 
@@ -18,42 +22,102 @@ public class HW4 {
     byte[] cipherText = Files.readAllBytes(Paths.get("P1_cipher.txt"));
     
     // BEGIN SOLUTION
-    // byte[] plainText = decrypted cipherText;
+
+    // initialize key and IV
+    SecretKeySpec secretKeySpec = new SecretKeySpec(key,"AES");
+    IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+    // create and initialize cipher given the specifications
+    Cipher cipher = Cipher.getInstance("AES/CBC/ISO10126Padding");
+    cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+    // decript cipherText using the created cipher
+    byte[] plainText = cipher.doFinal(cipherText);
+
     // END SOLUTION
     
-    //System.out.println(new String(plainText, StandardCharsets.UTF_8));
+    System.out.println(new String(plainText, StandardCharsets.UTF_8));
   }
   
   static void P2() throws Exception {
     // BEGIN SOLUTION
-    // byte[] P1_plaintext = "plaintext from P1".getBytes(StandardCharsets.UTF_8);
-    // byte[] key = generated digest
+    byte[] P1_plaintext = "The quick brown fox jumps over the lazy dog.".getBytes(StandardCharsets.UTF_8);
+    // create digest instance
+    MessageDigest md5 = MessageDigest.getInstance("MD5");
+    // update with the plaintext
+    md5.update(P1_plaintext);
+    //hash
+    byte[] key = md5.digest();
     // END SOLUTION
     
-    //System.out.println(Arrays.toString(key));
+    System.out.println(Arrays.toString(key));
   }
   
   static void P3() throws Exception {
     byte[] cipherBMP = Files.readAllBytes(Paths.get("P3_cipher.bmp"));
     
     // BEGIN SOLUTION
-    // byte[] key = bytes from P2 (the MD5 digest);
-    // byte[] plainBMP = decrypted cipherBMP;
+    byte[] key = new byte[] {-28, -39, 9, -62, -112, -48, -5, 28, -96, 104, -1, -83, -33, 34, -53, -48};
+    // create key spec
+    SecretKeySpec secretKeySpec = new SecretKeySpec(key,"AES");
+    // create and initialize cipher given the specifications, no IV needed for ECB
+    Cipher cipher = Cipher.getInstance("AES/ECB/ISO10126Padding");
+    cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+    // decrypt
+    byte[] plainBMP = cipher.doFinal(cipherBMP);
     // END SOLUTION
-    
-   // Files.write(Paths.get("P3_plain.bmp"), plainBMP);
+
+   // System.out.println(Arrays.toString(plainBMP));
   }
+
+
+
   
   // of course, you can solve P4 by writing some code
   // or, you can do it manually. Basically, it is your choice!
-  //static void P4() throws Exception {
-  //}
+  static void P4() throws Exception {
+
+    /**
+     * Since the file is encrypted the file will not open since
+     * the header is unreadble, not because of the data. The data does
+     * not to be decrypted becasue it was encrypted using ECB which
+     * can preserve patterns in the data. Since we know that the file
+     * is the exact same size as P3 image, we can use the header that
+     * was decrypted in P3. I took the 14 byte header and found the byte
+     * offset (bytes 10-14) which is where the image data begins. I took
+     * header from P3 wrote it into a new file, and then offset the encrypted
+     * data by the amount specified and filled the file with the remaining
+     * encrypted data. While the image is not exact, paterns reveal key
+     * patterns about the file. In this case the image produced 5 numbers:
+     *
+     *                    -65 6 -39 -69 115
+     *
+     */
+
+    // get images data
+    byte[] plainP3 = Files.readAllBytes(Paths.get("P3_plain.bmp"));
+    byte[] cipherP4 = Files.readAllBytes(Paths.get("P4_cipher.bmp"));
+
+    // get size and convert to unsigned int
+    byte offset = plainP3[10];
+    int startOfPixels = Byte.toUnsignedInt(offset);
+
+    // copy header from P3 and pixels from P4
+    byte[] header = Arrays.copyOfRange(plainP3, 0, startOfPixels);
+    byte[] pixels = Arrays.copyOfRange(cipherP4, startOfPixels, plainP3.length);
+
+    // write header and append pixels
+    Files.write(Paths.get("P4_plain.bmp"), header);
+    Files.write(Paths.get("P4_plain.bmp"), pixels, StandardOpenOption.APPEND);
+
+  }
   
   public static void main(String[] args) {
     try {  
       P1();
-      //P2();
-      //P3();
+      P2();
+      P3();
+      P4();
     } catch (Exception e) {
       e.printStackTrace();
     } 
